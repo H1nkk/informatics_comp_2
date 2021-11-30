8535f09a42407e527053af5435589b1b8a9bf1c7
H1nkk <88589411+H1nkk@users.noreply.github.com>
2021-11-30 20:00:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+var letters = /^[A-Za-zА-Яа-я -]+$/;
+
+function formValidation() { 
+    var uname = document.getElementById("name")
+    var usecname = document.getElementById("secname")
+    var uemail = document.getElementById("email")
+    var uphnumber = document.getElementById("phnumber")
+    var upassword = document.getElementById("password")
+
+    if(uname_validation(uname)) {
+        if(usecname_validation(usecname)) {
+            if(uemail_validation(uemail)) {
+                if(uphnumber_validation(uphnumber)) {
+                    if(upassword_validation(upassword)) {
+
+                    }
+                }
+            }
+        }
+    }
+    return false
+}
+
+function uname_validation(uname) { // проверка на валидность имени пользователя
+
+var uname_len = uname.value.length;
+
+
+
+if (uname_len == 0 || !(uname.value.match(letters)) || uname_len > 100) {
+    alert("Введите корректное имя.")
+    return false
+}
+else {
+    return true
+}
+}
+
+function usecname_validation(usecname) { // проверка на валидность имени пользователя
+
+var usecname_len = usecname.value.length;
+
+
+if (usecname_len == 0 || usecname_len > 100 || !(usecname.value.match(letters))) {
+    alert("Введите корректную фамилию.")
+    return false
+}
+else {
+    return true
+}
+
+}
+
+/*
+function uemail_validation(uemail) {
+
+var uemail_len = uemail.value.length;
+
+if (uemail_len < 2)
+
+}
+*/
